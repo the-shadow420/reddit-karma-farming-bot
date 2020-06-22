37f9589b2013106808264f23e2d64e46ed21f412
fghjkgcfxx56u <66923827+fghjkgcfxx56u@users.noreply.github.com>
2020-06-22 23:29:16 +0200

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -2,5 +2,5 @@
 REDDIT_CLIENT_ID="SPutZuyghrh-jQ"
 REDDIT_SECRET="v79XznXGePsSCds3PEDAWB5nH4I"
 REDDIT_USERNAME="taskuratik"
-REDDIT_PASSWORD="Gatto661"
+REDDIT_PASSWORD="Gatto661!?"
 REDDIT_USER_AGENT="animewow by /u/taskuratik" # This can be anything you want.
