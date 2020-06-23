3b585d531a763c7f7707eb52d805cc7de126de56
fghjkgcfxx56u <66923827+fghjkgcfxx56u@users.noreply.github.com>
2020-06-23 11:39:33 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -50,7 +50,7 @@ SHOW_SLEEP_LOGGING = False
 # You can add multiple schedules to be run after x days of the bots life
 USE_SLEEP_SCHEDULE = False
 BOT_SCHEDULES = [
-  {"days": 0, "schedule": [((4,00),(5,00)), ((17,30),(19,30))]},
+  {"days": 0, "schedule": [((2,00),(7,00)), ((15,30),(21,30))]},
   {"days": 4, "schedule": [((8,00),(10,00)), ((20,30),(23,20))]},
   ]
 
@@ -59,7 +59,7 @@ if os.environ.get('PORT'):
   if not os.environ.get('NOSCHEDULE'):
     USE_SLEEP_SCHEDULE = True
     BOT_SCHEDULES = [
-      {"days": 0, "schedule": [((4,00),(5,00)), ((17,30),(19,30))]},
+      {"days": 0, "schedule": [((2,00),(7,00)), ((15,30),(21,30))]},
       {"days": 4, "schedule": [((8,00),(10,00)), ((20,30),(23,20))]},
       {"days": 12, "schedule": [((9,00),(12,30)), ((18,00),(22,00))]},
       ]
